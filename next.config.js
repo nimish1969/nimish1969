13f16f3cb7c4ff5026768793d3547bf742ea6cc5
Paurav Shah <pauravshah@Pauravs-Laptop.local>
2024-11-25 04:17:25 +0530

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 // next.config.js
 module.exports = {
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/nimish1969/' : '',
-    basePath: '/nimish1969',
-    trailingSlash: true,
+    trailingSlash: true
   };
   
