c4ef2cfb781e862f0b15232cf8cbbaf97b541295
Paurav Shah <pauravshah@Pauravs-Laptop.local>
2024-11-25 03:42:37 +0530

    add gh-pages config

---
+++ next.config.js
@@ -0,0 +1,7 @@
+// next.config.js
+module.exports = {
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/nimish1969/' : '',
+    basePath: '/nimish1969',
+    trailingSlash: true,
+  };
+  
